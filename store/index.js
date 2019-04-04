e51a117403b7c7c66b0ee36b33110222eaeb35e0
achille T <achilletuglo12@gmail.com>
2019-04-04 08:02:49 +0200

    nshop

---
+++ store/index.js
@@ -0,0 +1,39 @@
+import fireApp from '@/plugins/firebase'
+
+export const state = () => ({
+  user: null,
+  counter: 0
+})
+
+export const mutations = {
+  setCounter(state, payload) {
+    state.counter = state.counter + payload
+  }
+}
+
+export const actions = {
+  fireTest() {
+    const payload = {
+      one: 'Apple',
+      two: 'Orange'
+    }
+    fireApp
+      .database()
+      .ref('testdb')
+      .push(payload)
+      .then(() => {
+      })
+      .catch(() => {
+      })
+  },
+
+  increment({ commit }, payload) {
+    commit('setCounter', payload)
+  }
+}
+
+export const getters = {
+  counter(state) {
+    return state.counter
+  }
+}
